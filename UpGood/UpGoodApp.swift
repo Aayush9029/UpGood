f047e7162b5c2f6842ef44b0a16d1696648752f3
Aayush9029 <aayushpokharel36@gmail.com>
2023-01-09 13:33:37 -0500

    Initial Commit

---
+++ UpGood/UpGoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpGoodApp.swift
+//  UpGood
+//
+//  Created by Aayush Pokharel on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct UpGoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
